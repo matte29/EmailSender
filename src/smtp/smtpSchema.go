d7547cb86fb72e25672e0017438b8b0c6a2f8af5
Matthew Ellis <matthewaellis29@gmail.com>
2021-08-10 13:00:08 -0400

    Changed name of smtpSchema

--- src/smtp/smtpSchema.go
+++ src/smtp/smtpSchema.go
@@ -1,13 +1,13 @@
-package SMTP
+package sending
 
-type SMTP struct {
+type smtpInfo struct {
 	host     string
 	port     string
 	from     string
 	password string
 }
 
-func (s *SMTP) Init(host string, port string, from string, password string) {
+func (s *smtpInfo) Init(host string, port string, from string, password string) {
 	s.host = host
 	s.port = port
 	s.from = from
