cd9f0407b2cce4b4ecad26d3c70a7b24ff7ebfee
Matthew Ellis <matthewaellis29@gmail.com>
2021-11-15 22:31:44 -0500

    Updated sendMail with how it gets To information

--- src/smtp/sendMail.go
+++ src/smtp/sendMail.go
@@ -5,12 +5,16 @@ import (
 	"net/smtp"
 )
 
-// Sends email to recipents in "to" array
+// Sends email to recipents in s.To array
 // Takes smtpInfo, array of recipents, bytes.Bufffer,
 func (s *SMTPInfo) SendEmail(index int) {
 
+	var stringArray []string
+
+	stringArray = append(stringArray, s.To[index])
+
 	auth := smtp.PlainAuth("", s.From, s.Password, s.Host)
-	err := smtp.SendMail(s.Host+":"+s.Port, auth, s.From, s.To, []byte(s.Body))
+	err := smtp.SendMail(s.Host+":"+s.Port, auth, s.From, stringArray, []byte(s.Body))
 	if err != nil {
 		fmt.Println(err)
 		return
