c89afb171cad39b3690aa06e71a1beeceaefa2c4
Matthew Ellis <matthewaellis29@gmail.com>
2021-11-15 22:31:07 -0500

    Updated setBody with documentation

--- src/smtp/setBody.go
+++ src/smtp/setBody.go
@@ -7,6 +7,13 @@ import (
 
 // Adds template to a bytes.Buffer.  Takes the location of the HTML template,
 // an interface{} that can be blank
+//
+//		Location: This is the location of the HTML file.
+//		data: This is an interface, will be implemented further in the future,
+//			  currently just set to the email address
+//
+//		TODO, Implement the "data: interface" based on user input
+
 func (s *SMTPInfo) SetBody(location string, index int, data interface{}) error {
 
 	t, err := template.ParseFiles(location)
