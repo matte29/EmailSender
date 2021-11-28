e594404d962b4698f6b0c89cf697d93e643bc123
Matthew Ellis <matthewaellis29@gmail.com>
2021-11-28 18:59:53 -0500

    Slightly Changed home.go
    Main reason for change was a bug that was solved because  s (the SMTPInfo) was created as a pointer for some reason and was getting a nil error when data was sent to it. The two defer statements that have warnings are required and will break the  upload of the second file upload.

--- src/controller/home.go
+++ src/controller/home.go
@@ -10,9 +10,119 @@ import (
 	"github.com/matte29/EmailSender/src/smtp"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
+func post(w http.ResponseWriter, r *http.Request) {
 
-	var s *smtp.SMTPInfo
+	var s smtp.SMTPInfo
+
+	// Filename for CSV file
+	var csvFileName string = "tmp/"
+
+	// Filename for HTML file
+
+	var htmlFileName string = "tmp/"
+
+	//! Maybe change this to larger size
+	// Maximum upload of 10 MB files
+	r.ParseMultipartForm(10 << 20)
+
+	// Get data from the HTML Form
+	s.From = r.FormValue("from")
+	s.Subject = r.FormValue("subject")
+	s.Password = r.FormValue("password")
+	s.Host = r.FormValue("host")
+	s.Port = r.FormValue("port")
+
+	// Get and Upload HTML File
+	file, handler, err := r.FormFile("html")
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
+	}
+
+	defer file.Close()
+	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	fmt.Printf("File Size: %+v\n", handler.Size)
+	fmt.Printf("MIME Header: %+v\n", handler.Header)
+	htmlFileName += handler.Filename
+
+	// Create file
+	dst, err := os.Create("tmp/" + handler.Filename)
+	defer dst.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Copy the uploaded file to the created file on the filesystem
+	if _, err := io.Copy(dst, file); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	//--------------------------------------------------
+
+	// Get and Upload CSV File
+	csvFile, handler, err := r.FormFile("csv")
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
+	}
+
+	defer csvFile.Close()
+	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	fmt.Printf("File Size: %+v\n", handler.Size)
+	fmt.Printf("MIME Header: %+v\n", handler.Header)
+	csvFileName += handler.Filename
+
+	// Create file
+	dst2, err := os.Create("tmp/" + handler.Filename)
+	defer dst2.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Copy the uploaded file to the created file on the filesystem
+	if _, err := io.Copy(dst2, csvFile); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	//--------------------------------------------------
+
+	fmt.Printf("Subject = %s\n", s.Subject)
+	fmt.Printf("From = %s\n", s.From)
+	fmt.Printf("Password = %s\n", s.Password)
+	fmt.Printf("Host = %s\n", s.Host)
+	fmt.Printf("Port = %s\n", s.Port)
+	fmt.Println("Successfully Uploaded File")
+
+	// Calls ReadCsvFile to get the emails from the CSV file
+	s.To = csv.ReadCsvFile(csvFileName)
+
+	if len(s.To) == 0 {
+		fmt.Println("Error No Emails were recieved.")
+
+		http.Error(w,
+			"Error No Emails were recieved in the CSV File",
+			http.StatusInternalServerError)
+
+		return
+	}
+
+	// TODO: Change to the concurent way using waitgroup
+	// Calls setBody and then sends the email to the email address
+	for i, f := range s.To {
+		s.SetBody(htmlFileName, i, f)
+
+		s.SendEmail(i)
+	}
+
+	fmt.Println("Successfully Sent Emails")
+
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
 
 	if r.URL.Path != "/" {
 		ErrorHandler(w, r, http.StatusNotFound)
@@ -25,112 +135,7 @@ func Home(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Get \"/\" was called.")
 	case "POST":
 		fmt.Println("Post \"/\" was called.")
-
-		// Filename for CSV file
-		var csvFileName string = "tmp/"
-
-		// Filename for HTML file
-
-		var htmlFileName string = "tmp/"
-
-		if err := r.ParseMultipartForm(10 << 20); err != nil {
-			fmt.Println("Error ParseMultipartForm: ", err)
-			return
-		}
-
-		// Upload HTML File
-		htmlFile, handler, err := r.FormFile("file")
-		if err != nil {
-			fmt.Println("Error Retrieving the File")
-			fmt.Println(err)
-			return
-		}
-
-		defer htmlFile.Close()
-
-		htmlFileName += handler.Filename
-
-		dst, err := os.Create("tmp/" + handler.Filename)
-		defer dst.Close()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		// Copy the uploaded file to the created file on the filesystem
-		if _, err := io.Copy(dst, htmlFile); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		//--------------------------------------------------
-
-		// Upload CSV File
-		csvFile, csvHandler, err := r.FormFile("csv")
-		if err != nil {
-			fmt.Println("Error Retrieving the File")
-			fmt.Println(err)
-			return
-		}
-
-		defer csvFile.Close()
-
-		csvFileName += csvHandler.Filename
-
-		copy, err := os.Create("tmp/" + csvHandler.Filename)
-		defer copy.Close()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		// Copy the uploaded file to the created file on the filesystem
-		if _, err := io.Copy(copy, csvFile); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		//--------------------------------------------------
-
-		//! Something is wrong with getting these values
-		s.From = r.FormValue("from")
-		fmt.Printf("From = %s\n", s.From)
-
-		s.Subject = r.FormValue("subject")
-		fmt.Printf("Subject = %s\n", s.Subject)
-
-		s.Password = r.FormValue("password")
-
-		s.Host = r.FormValue("host")
-		fmt.Printf("Host = %s\n", s.Host)
-
-		s.Port = r.FormValue("port")
-		fmt.Printf("Port = %s\n", s.Port)
-
-		// Calls ReadCsvFile to get the emails from the CSV file
-		s.To = csv.ReadCsvFile(csvFileName)
-
-		if len(s.To) == 0 {
-			fmt.Println("Error No Emails were recieved.")
-
-			http.Error(w,
-				"Error No Emails were recieved in the CSV File",
-				http.StatusInternalServerError)
-
-			return
-		}
-		// var wg sync.WaitGroup
-
-		// var counter int = 0
-		// for {
-		// 	wg.Add(1)
-
-		// }
-		// TODO SetBody, SendMail
-		for i, f := range s.To {
-			s.SetBody(htmlFileName, i, f)
-
-			s.SendEmail(i)
-		}
-
+		post(w, r)
 	default:
 		http.Redirect(w, r, "/404", 404)
 	}
